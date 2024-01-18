bee9f322bb73c5668cf95655d00dcabb420f86f2
FzrFernando <74764616+FzrFernando@users.noreply.github.com>
2024-01-18 21:19:36 +0100

    AsyncPipe

--- src/app/minions/minions.component.ts
+++ src/app/minions/minions.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Minion } from '../interfaces/minions';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { MinionService } from '../services/minion.service';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-minions',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, AsyncPipe],
   templateUrl: './minions.component.html',
   styleUrl: './minions.component.css'
 })
@@ -16,17 +17,20 @@ export class MinionsComponent implements OnInit,OnChanges{
   @Input() mensaje!: string;
   @Output() eventoClic = new EventEmitter<void>();
   // @Input() minions: Minion[] = [];
-  minions : Minion[] = [];
+  minions$! : Observable<Minion[]>;
   @Input() searchTerm: string = '';
 
-  constructor(private minionsService: MinionService){
-  }
+  constructor(private minionsService: MinionService){}
 
   ngOnInit(){
-    this.minionsService.getMinions().subscribe({
-      next: (minions) => this.minions = minions;
-      error: (error) =>
-    });
+    // this.minionsService.getMinions().subscribe({
+    //   next: (minions) => {
+    //     this.minions = minions;
+    //     this.error = false;
+    //   },
+    //   error: (error) => this.error = true
+    // })
+    this.minions$ = this.minionsService.getMinions();
   }
 
   enviarEvento() {
@@ -34,8 +38,8 @@ export class MinionsComponent implements OnInit,OnChanges{
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.minionsService.getMinions().subscribe({
-      next: (minions) => this.minions = minions
-    });
+    // this.minionsService.getMinions().subscribe({
+    //   next: (minions) => this.minions = minions
+    // });
   }
 }
